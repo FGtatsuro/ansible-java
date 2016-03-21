aafc6ec08fcf9b77dadfd6300bd41b2fc5aca725
FGtatsuro <fujiistorage@gmail.com>
2016-03-22 01:06:01 +0900

    Monkey patch related to send_file of Docker backend is no longer needed.

--- spec/spec_helper_docker.rb
+++ spec/spec_helper_docker.rb
@@ -4,23 +4,6 @@ set :backend, :docker
 set :docker_url, ENV['DOCKER_HOST']
 set :docker_container, ENV['TARGET_HOST']
 
-module Specinfra
-  module Backend
-    class Docker
-      alias :__send_file__ :send_file
-      def send_file(from, to)
-        if @container && @base_image.nil?
-          # This needs Docker >= 1.8
-          @container.archive_in(from, to)
-        else
-          __send_file__(from, to)
-        end
-      end
-      private :__send_file__
-    end
-  end
-end
-
 module SpecHelper
   def create_file(filename, content)
       File.write(filename, content)
